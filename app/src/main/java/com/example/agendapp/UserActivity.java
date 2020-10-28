e69fac0d0a88432ae6e8e48a337801fdba648e9b
crramirezma <crramirezma@unal.edu.co>
2020-10-28 15:15:02 -0500

    reiniciando

---
+++ app/src/main/java/com/example/agendapp/UserActivity.java
@@ -0,0 +1,18 @@
+package com.example.agendapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class UserActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user);
+    }
+
+    public void modificarListener(View view) {
+    }
+}
