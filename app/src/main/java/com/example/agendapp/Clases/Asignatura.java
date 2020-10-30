8086c3a944554fcbc922bcb64e1db6c8d24a4bb2
alramosp <alramosp@unal.edu.co>
2020-10-30 18:18:46 -0500

    Cambios Clases (Asignatura y Subtema)

--- app/src/main/java/com/example/agendapp/Clases/Asignatura.java
+++ app/src/main/java/com/example/agendapp/Clases/Asignatura.java
@@ -1,9 +1,30 @@
 package com.example.agendapp.Clases;
 
+import java.util.ArrayList;
+
 public class Asignatura {
     private String nombre;
     private int creditos;
     private double tiempoEstudio;
+
+    public Asignatura(String nombre, int creditos, double tiempoEstudio) {
+        this.nombre=nombre;
+        this.creditos=creditos;
+        this.tiempoEstudio=tiempoEstudio;
+    }
+
+    private ArrayList<Subtema> subtemas=new ArrayList<>();
+
+    public ArrayList<Subtema> getsubtemas() {
+        return subtemas;
+    }
+    public void setSubtemas(ArrayList<Subtema> subtemas) {
+        this.subtemas = subtemas;
+    }
+    public void addSubtema(Subtema subtema){
+        subtemas.add(subtema);
+    }
+
 
     public String getNombre() {
         return nombre;
--- app/src/main/java/com/example/agendapp/Clases/Subtema.java
+++ app/src/main/java/com/example/agendapp/Clases/Subtema.java
@@ -1,4 +1,28 @@
 package com.example.agendapp.Clases;
 
 public class Subtema {
+    private Asignatura asignaturaSubtema;
+    private String nombreSubtema;
+
+    public Subtema(String nombreSubtema,Asignatura asignaturaSubtema) {
+        this.asignaturaSubtema=asignaturaSubtema;
+        this.nombreSubtema=nombreSubtema;
+    }
+
+    public String getNombreSubtema() {
+        return nombreSubtema;
+    }
+
+    public void setNombreSubtema(String nombreSubtema) {
+        this.nombreSubtema= nombreSubtema;
+    }
+
+    public Asignatura getasignaturaSubtema() {
+        return asignaturaSubtema;
+    }
+
+    public void setAsignaturaSubtema(Asignatura asignaturaSubtema) {
+        this.asignaturaSubtema= asignaturaSubtema;
+    }
+
 }
