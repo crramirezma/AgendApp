c8b7cd3f8e430950755ae833c5b543f631d06910
alramosp <alramosp@unal.edu.co>
2020-11-24 18:52:16 -0500

    Clase Tarea :100:

--- app/src/main/java/com/example/agendapp/Clases/Tarea.java
+++ app/src/main/java/com/example/agendapp/Clases/Tarea.java
@@ -1,5 +1,37 @@
 package com.example.agendapp.Clases;
 
 public class Tarea {
+
+    private String nombreTarea;
+    private int estadoTarea;
+    // 1 : Tarea Completada
+    // 2 : Tarea Pendiente
+    // 3 : Tarea en Proceso
     
+
+
+
+    public Tarea(String nombreTarea,int estadoTarea){
+        this.nombreTarea = nombreTarea;
+        this.estadoTarea = estadoTarea;
+    }
+
+
+    public String getnombreTarea() {
+        return nombreTarea;
+    }
+
+
+    public void setnombreTarea(String nombreTarea) {
+        this.nombreTarea=nombreTarea;
+    }
+
+    public int getestadoTarea() {
+        return estadoTarea;
+    }
+
+
+    public void setestadoTarea(int estadoTarea) {
+        this.estadoTarea=estadoTarea;
+    }
 }
