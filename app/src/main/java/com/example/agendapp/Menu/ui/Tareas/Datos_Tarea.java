1d7683d362e082a7e97b8338c6c510293565c8db
alramosp <alramosp@unal.edu.co>
2020-11-27 07:35:35 -0500

    Vista de Datos Tarea :alien:

---
+++ app/src/main/java/com/example/agendapp/Menu/ui/Tareas/Datos_Tarea.java
@@ -0,0 +1,15 @@
+package com.example.agendapp.Menu.ui.Tareas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import com.example.agendapp.R;
+
+public class Datos_Tarea extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_datos__tarea);
+    }
+}
